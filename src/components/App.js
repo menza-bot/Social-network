ff6064a805b3fa9a32e3ee25eaf883a77972dd96
Muhsin <muhsin.2002@mail.ru>
2020-09-22 16:08:29 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import logo from './src/logo.svg';
+//import './src/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Some text </div>
+      <div>Some text and something else</div>
+      <span className="matura"></span>
+    </div>
+  );
+}
+
+export default App;
